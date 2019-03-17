aefb530e3b84805a25febcf221377c81513eb3d4
Hendrik Walberg <hendrik.walberg@chasacademy.se>
2019-03-17 20:26:04 +0100

    Updates

---
+++ precache-manifest.6867042445a5349803b20c8bd8c14478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "767b9729f7a33b885dbc",
+    "url": "/inweather/static/css/main.301fe876.chunk.css"
+  },
+  {
+    "revision": "767b9729f7a33b885dbc",
+    "url": "/inweather/static/js/main.1bf2c9f4.chunk.js"
+  },
+  {
+    "revision": "0e2f6871c7d00a5fa5c8",
+    "url": "/inweather/static/js/runtime~main.7d59899f.js"
+  },
+  {
+    "revision": "8d0e8f209740eac48c97",
+    "url": "/inweather/static/js/2.c94ebf9b.chunk.js"
+  },
+  {
+    "revision": "4d9ea840f073a75eb8c11b6cd3decd3f",
+    "url": "/inweather/index.html"
+  }
+];
